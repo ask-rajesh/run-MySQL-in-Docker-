ffb2ffde880c69e27856e05167b614ac26df2038
Rajesh Kumar Sah <50516148+ask-rajesh@users.noreply.github.com>
2024-09-23 15:54:27 +0530

    connect MySQL

---
+++ connect_mysql.py
@@ -0,0 +1,29 @@
+import mysql.connector
+from mysql.connector import Error
+
+def read_data():
+    try:
+        connection = mysql.connector.connect(
+            host='127.0.0.1',
+            port=3306,
+            user='root',  
+            password='rootpassword',  
+            database='mydatabase'
+        )
+        if connection.is_connected():
+            print("Connected to MySQL database")
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM student")
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+if __name__ == "__main__":
+    read_data()
